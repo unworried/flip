f1a3686df6a8fd710782d4913e9af174d3da3001
unworried <unworried@tuta.io>
2024-01-30 19:39:00 +1000

    add: parser base

---
+++ src/parser.rs
@@ -0,0 +1,35 @@
+use crate::lexer::{Lexer, Token};
+use std::mem;
+
+pub struct Parser<'a> {
+    lexer: &'a mut Lexer,
+    current_token: Token,
+    next_token: Token,
+}
+
+
+impl<'a> Parser<'a> {
+    pub fn new(lexer: &'a mut Lexer) -> Self {
+        let current_token = lexer.next_token();
+        let next_token = lexer.next_token();
+
+        Self {
+            lexer,
+            current_token,
+            next_token,
+        }
+    }
+    
+    pub fn step(&mut self) {
+        // Cheaper than cloning
+        self.current_token = self.lexer.next_token();
+        mem::swap(&mut self.current_token, &mut self.next_token);
+    }
+}
+
+pub trait Parse 
+    where Self: Sized 
+{
+    fn parse(parser: &mut Parser) -> Self;
+}
+         
