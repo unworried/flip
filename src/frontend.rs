11a74f358c9546b49f5cd9f73891057c89edf72e
unworried <unworried@tuta.io>
2024-02-05 14:22:18 +1000

    abstracted frontend flow into interal file to reduce exports

---
+++ src/frontend.rs
@@ -0,0 +1,24 @@
+use crate::{
+    cache::Cache, diagnostics::DiagnosticBag, error::Result, lexer::Lexer, parser::Parser,
+    resolver::Resolver, source::Source,
+};
+
+pub fn check(input: &str) -> Result<()> {
+    let diagnostics = DiagnosticBag::new();
+    let cache = Cache::new(diagnostics.clone());
+
+    // Fix to make lexer take src
+    let source = Source::new(input.to_string());
+    let mut lexer = Lexer::new(input.to_string());
+    let mut parser = Parser::new(&mut lexer, diagnostics.clone());
+
+    let result = parser.parse();
+    println!();
+    println!("{}", result);
+
+    let mut resolver = Resolver::new(cache);
+    resolver.search(&result);
+
+    diagnostics.borrow().check(&source)?;
+    Ok(())
+}
