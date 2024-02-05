df9bb3fa3641d5780be78ed6c8b1a031fdb1ea0d
unworried <unworried@tuta.io>
2024-02-05 13:13:10 +1000

    basic single run name resolver working

--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -1,31 +1,31 @@
 use crate::{
-    diagnostics::DiagnosticsCell,
+    cache::Cache,
     parser::{
-        ast::{statement::Local, Ast, Expr, Ident},
-        visitor::{Visitor, Walkable},
+        ast::{statement::Local, Ast, Ident},
+        visitor::Visitor,
     },
 };
 
 use self::scope::Scope;
 
-mod idxvec;
 pub mod scope;
 
 pub struct Resolver {
-    scopes: Vec<Scope>,
-    diagnostics: DiagnosticsCell,
+    cache: Cache,
+    scope: Scope,
 }
 
 impl Resolver {
-    pub fn new(scopes: Vec<Scope>, diagnostics: DiagnosticsCell) -> Self {
+    pub fn new(cache: Cache) -> Self {
         Self {
-            scopes,
-            diagnostics,
+            cache,
+            scope: Scope::new(),
         }
     }
 
-    pub fn resolve(&mut self, ast: &Ast) {
+    pub fn search(&mut self, ast: &Ast) {
         self.visit_ast(ast);
+        //println!("{:?}", self.cache.definitions);
     }
 }
 
@@ -36,41 +36,43 @@ impl Resolver {
  * declared within the scope.
  */
 impl Visitor for Resolver {
-    fn visit_local(&mut self, local: &Local) {
-        let scope = self.scopes.last_mut().unwrap();
+    fn visit_declaration(&mut self, local: &Local) {
         let pattern = &local.pattern.0;
 
-        if scope.variables.iter().any(|v| v == pattern) {
-            self.diagnostics
+        if self.scope.check_variable(pattern) {
+            self.cache
+                .diagnostics
                 .borrow_mut()
                 .variable_already_declared(pattern, &local.pattern.1);
             return;
         }
 
-        scope.declare_variable(pattern.to_owned());
+        let id = self.scope.declare_variable(pattern.to_owned());
+        self.cache.push_declartion(id, local);
     }
 
-    fn visit_assignment(&mut self, ident: &Ident, expr: &Expr) {
-        let scope = self.scopes.last_mut().unwrap();
-        let pattern = &ident.0;
+    fn visit_assignment(&mut self, local: &Local) {
+        let pattern = &local.pattern.0;
 
-        if scope.variables.iter().all(|v| v != pattern) {
-            self.diagnostics
+        if !self.scope.check_variable(pattern) {
+            self.cache
+                .diagnostics
                 .borrow_mut()
-                .undeclared_variable_assignment(pattern, &ident.1);
-            expr.walk(self);
+                .undeclared_variable_assignment(pattern, &local.pattern.1);
+            self.visit_local(local);
             return;
         }
 
-        scope.declare_variable(pattern.to_owned());
+        let id = self.scope.declare_variable(pattern.to_owned());
+        self.cache.push_definition(id, local);
     }
 
     fn visit_variable(&mut self, ident: &Ident) {
-        let scope = self.scopes.last_mut().unwrap();
         let pattern = &ident.0;
 
-        if scope.variables.iter().all(|v| v != pattern) {
-            self.diagnostics
+        if !self.scope.check_variable(pattern) {
+            self.cache
+                .diagnostics
                 .borrow_mut()
                 .reference_before_assignment(pattern, &ident.1);
         }
--- src/resolver/scope.rs
+++ src/resolver/scope.rs
@@ -1,29 +1,35 @@
-use crate::idx;
-use crate::resolver::idxvec::Idx;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use super::idxvec::IdxVec;
+use crate::cache::DefinitionId;
 
-idx!(VariableIdx);
+pub type VarCell = Rc<RefCell<HashMap<String, DefinitionId>>>;
 
 #[derive(Debug)]
 pub struct Scope {
-    pub variables: IdxVec<VariableIdx, String>,
-}
-
-impl Scope {
-    pub fn new() -> Self {
-        Self {
-            variables: IdxVec::new(),
-        }
-    }
-
-    pub fn declare_variable(&mut self, name: String) -> VariableIdx {
-        self.variables.push(name)
-    }
+    pub variables: VarCell,
 }
 
 impl Default for Scope {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl Scope {
+    pub fn new() -> Self {
+        Self {
+            variables: Rc::new(RefCell::new(HashMap::new())),
+        }
+    }
+
+    pub fn check_variable(&self, name: &str) -> bool {
+        self.variables.borrow().get(name).is_some()
+    }
+
+    pub fn declare_variable(&mut self, name: String) -> DefinitionId {
+        let mut variable_store = self.variables.borrow_mut();
+        let len = variable_store.len();
+        variable_store.insert(name, DefinitionId(len));
+        DefinitionId(len)
     }
 }
